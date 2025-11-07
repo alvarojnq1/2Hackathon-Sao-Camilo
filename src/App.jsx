e2a292525d331c518f173263b6448b8b958896e7
victorhotpinho7@gmail.com <rudolf@maua.br>
2025-11-07 11:10:48 -0300

    geral

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './global.css'
+import Login from './pages/login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <body>
-  <h1 class="text-3xl font-bold underline text-red-300">
-    Hello world!
-  </h1>
-</body>
+      <Login/>
+    </body>
   )
 }
 
